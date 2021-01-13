7010cbc906e7c921e63e9aa07e39f8d02a8a239d
Ivan <56699565+ipintar@users.noreply.github.com>
2021-01-13 17:06:47 +0100

    test

---
+++ src/app/customers/customers.module.ts
@@ -0,0 +1,18 @@
+import { NgModule }      from '@angular/core';
+import { FormsModule }      from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { CustomersComponent }  from './customers.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { FilterTextboxComponent } from './filter-textbox/filter-textbox.component';
+import { SharedModule } from '../shared/shared.module';
+import { CustomersRoutingModule } from './customers-routing.module';
+
+
+
+@NgModule({
+  imports:      [ CommonModule, SharedModule, FormsModule, CustomersRoutingModule ],
+  declarations: [ CustomersComponent, CustomersListComponent, FilterTextboxComponent ],
+  exports: [ CustomersComponent ]
+})
+export class CustomersModule { }
